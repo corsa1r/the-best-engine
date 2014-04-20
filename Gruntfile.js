bd3e6d678971ee8f66dec2fd164e2b6930c1a2b3
Svetoslav Mishkov <svetoslav.mishkov@gmail.com>
2014-04-20 16:23:57 +0300

    Added file copy task to the build sequence.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,30 @@ module.exports = function(grunt) {
 
         clean: ['dist/*'],
 
+        copy: {
+            dev: {
+                files: [
+                     {expand: true, cwd: 'src/', src: ['**'], dest: 'dist/', ext: '.js'},
+                     {expand: true, src: ['bower_components/**'], dest: 'dist/'}
+                ]
+            }
+        },
+
         karma: {
             unit: {
                 configFile: 'karma.conf.js'
             }
-        },
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
-    //grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('build:dev', ['clean', 'copy:dev']);
+    //grunt.registerTask('build:prod', ['clean', ...]);
+    grunt.registerTask('default', ['build:dev']);
 };
