8a3bed495f398dcb608dc15a6531f3d20b5aa0a5
Vladimir Enchev <vladimir.corsair@gmail.com>
2014-11-12 16:21:47 +0200

    Update InputRouter.js

--- src/engine/input/InputRouter.js
+++ src/engine/input/InputRouter.js
@@ -1,31 +1,57 @@
-;(function() {
-
+/**
+ * @author CORSAIR <vladimir.corsair@gmail.com>
+ * @author Apo <apomarinov@gmail.com>
+ * 
+ * @file implements InputRouter class
+ * @requires Scene class
+ * @description this class implements input source routing
+ * @see 
+ *  	var canvas = document.getElementById('canvas');
+ * 		var scene = new Scene(canvas, 500, 500);
+ * 		var inputRouter = new InputRouter(scene);
+ * 
+ * 		inputRouter.editor.mouse.on('output', function(event) {
+ *			//Do something with mouse in editor
+ *		});
+ * 
+ * 		inputRouter.client.mouse.on('output', function(event) {
+ *			//Do something with mouse in game
+ *		});
+ * 
+ * @version 12.11.2014a
+ */
+ ;(function() {
+	
 	define([
-		'src/engine/EventSet',
 		'src/engine/input/KeyboardInput',
 		'src/engine/input/MouseInput',
 		'src/engine/input/StateMap',
 		'src/engine/input/InputSet'
-		], function(EventSet, KeyboardInput, MouseInput, StateMap, InputSet) {
-
+		], function(KeyboardInput, MouseInput, StateMap, InputSet) {
+		
 		var InputRouter = function(scene) {
 			InputRouter.super.constructor.call(this);
 
 			this.editOn = true;
 			this.initInputs(scene);
 		};
-
-		InputRouter.extend(EventSet);
-
+		
+		/**
+		 * This method initializes the input sources
+		 * Then automaticly feeds them with StateMap
+		 * Then triggers the correct event in the corrent place depends on editOn field
+		 */
 		InputRouter.prototype.initInputs = function(scene) {
+			//Init input sources
 			var mouse = new MouseInput(scene.screen);
-			var mouseStateMap = new StateMap();
-
-			mouse.on('output', mouseStateMap.feed(MouseInput.buttonMap));
-
 			var keyboard = new KeyboardInput();
+			
+			//Init StateMaps
+			var mouseStateMap = new StateMap();
 			var keyboardStateMap = new StateMap();
-
+	
+			//Feeds maps
+			mouse.on('output', mouseStateMap.feed(MouseInput.buttonMap));
 			keyboard.on('output', keyboardStateMap.feed(KeyboardInput.keyMap));
 
 			mouseStateMap.on('output', (function(event) {
@@ -39,7 +65,15 @@
 			this.client = new InputSet();
 			this.editor = new InputSet();
 		};
-
+		
+		/**
+		 * This method fires an event
+		 * If this.editOn queals true, inputType is getted from 'editor' field which is InputSet
+		 * 
+		 * @param {String} inputType - 'mouse', 'keyboard' or 'touch'
+		 * @param {Object} event - this is output event from the input source
+		 * returns undefined
+		 */
 		InputRouter.prototype.triggerEvent = function(inputType, event) {
 			this[this.editOn ? 'editor' : 'client'][inputType].fire('output', event);
 		};
